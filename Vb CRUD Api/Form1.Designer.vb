8388be4aeab6c13ea0c55980ac6fed874e89cc03
Martin <martin020305@gmail.com>
2022-04-03 11:24:56 +0700

    tugas vb api~

---
+++ Vb CRUD Api/Form1.Designer.vb
@@ -0,0 +1,366 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle31 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle32 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle33 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle34 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle35 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle36 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn_delete = New System.Windows.Forms.Button()
+        Me.btn_update = New System.Windows.Forms.Button()
+        Me.btn_add = New System.Windows.Forms.Button()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txt_kelas = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txt_prodi = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txt_jurusan = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt_nama = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.nomor = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.npm = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.nama = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.jurusan = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.prodi = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.kelas = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btn_tambah = New System.Windows.Forms.Button()
+        Me.btn_cancel = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_npm = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1255, 41)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(146, 55)
+        Me.Button1.TabIndex = 18
+        Me.Button1.Text = "Refresh"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'btn_delete
+        '
+        Me.btn_delete.Location = New System.Drawing.Point(1255, 295)
+        Me.btn_delete.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_delete.Name = "btn_delete"
+        Me.btn_delete.Size = New System.Drawing.Size(146, 55)
+        Me.btn_delete.TabIndex = 15
+        Me.btn_delete.Text = "Delete"
+        Me.btn_delete.UseVisualStyleBackColor = True
+        '
+        'btn_update
+        '
+        Me.btn_update.Location = New System.Drawing.Point(1255, 211)
+        Me.btn_update.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_update.Name = "btn_update"
+        Me.btn_update.Size = New System.Drawing.Size(146, 62)
+        Me.btn_update.TabIndex = 14
+        Me.btn_update.Text = "Edit Data"
+        Me.btn_update.UseVisualStyleBackColor = True
+        '
+        'btn_add
+        '
+        Me.btn_add.Location = New System.Drawing.Point(1255, 126)
+        Me.btn_add.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_add.Name = "btn_add"
+        Me.btn_add.Size = New System.Drawing.Size(146, 55)
+        Me.btn_add.TabIndex = 13
+        Me.btn_add.Text = "Simpan"
+        Me.btn_add.UseVisualStyleBackColor = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(1279, 554)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(59, 23)
+        Me.Label6.TabIndex = 12
+        Me.Label6.Text = "Kelas"
+        '
+        'txt_kelas
+        '
+        Me.txt_kelas.Location = New System.Drawing.Point(1273, 582)
+        Me.txt_kelas.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_kelas.Name = "txt_kelas"
+        Me.txt_kelas.Size = New System.Drawing.Size(94, 30)
+        Me.txt_kelas.TabIndex = 11
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(966, 553)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(56, 23)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "Prodi"
+        '
+        'txt_prodi
+        '
+        Me.txt_prodi.Location = New System.Drawing.Point(958, 582)
+        Me.txt_prodi.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_prodi.Name = "txt_prodi"
+        Me.txt_prodi.Size = New System.Drawing.Size(276, 30)
+        Me.txt_prodi.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(635, 553)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(78, 23)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Jurusan"
+        '
+        'txt_jurusan
+        '
+        Me.txt_jurusan.Location = New System.Drawing.Point(626, 582)
+        Me.txt_jurusan.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_jurusan.Name = "txt_jurusan"
+        Me.txt_jurusan.Size = New System.Drawing.Size(306, 30)
+        Me.txt_jurusan.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(284, 553)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(166, 23)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Nama Mahasiswa"
+        '
+        'txt_nama
+        '
+        Me.txt_nama.Location = New System.Drawing.Point(269, 582)
+        Me.txt_nama.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_nama.Name = "txt_nama"
+        Me.txt_nama.Size = New System.Drawing.Size(346, 30)
+        Me.txt_nama.TabIndex = 5
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.nomor, Me.npm, Me.nama, Me.jurusan, Me.prodi, Me.kelas})
+        Me.DataGridView1.Location = New System.Drawing.Point(9, 123)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 62
+        Me.DataGridView1.Size = New System.Drawing.Size(1225, 406)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'nomor
+        '
+        DataGridViewCellStyle31.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nomor.DefaultCellStyle = DataGridViewCellStyle31
+        Me.nomor.HeaderText = "No."
+        Me.nomor.MinimumWidth = 8
+        Me.nomor.Name = "nomor"
+        Me.nomor.Width = 75
+        '
+        'npm
+        '
+        DataGridViewCellStyle32.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.npm.DefaultCellStyle = DataGridViewCellStyle32
+        Me.npm.HeaderText = "Nomor Pokok Mahasiswa"
+        Me.npm.MinimumWidth = 8
+        Me.npm.Name = "npm"
+        Me.npm.Width = 175
+        '
+        'nama
+        '
+        DataGridViewCellStyle33.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nama.DefaultCellStyle = DataGridViewCellStyle33
+        Me.nama.HeaderText = "Nama Mahasiswa"
+        Me.nama.MinimumWidth = 8
+        Me.nama.Name = "nama"
+        Me.nama.Width = 180
+        '
+        'jurusan
+        '
+        DataGridViewCellStyle34.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.jurusan.DefaultCellStyle = DataGridViewCellStyle34
+        Me.jurusan.HeaderText = "Jurusan"
+        Me.jurusan.MinimumWidth = 8
+        Me.jurusan.Name = "jurusan"
+        Me.jurusan.Width = 155
+        '
+        'prodi
+        '
+        DataGridViewCellStyle35.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.prodi.DefaultCellStyle = DataGridViewCellStyle35
+        Me.prodi.HeaderText = "Prodi"
+        Me.prodi.MinimumWidth = 8
+        Me.prodi.Name = "prodi"
+        Me.prodi.Width = 200
+        '
+        'kelas
+        '
+        DataGridViewCellStyle36.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.kelas.DefaultCellStyle = DataGridViewCellStyle36
+        Me.kelas.HeaderText = "Kelas"
+        Me.kelas.MinimumWidth = 8
+        Me.kelas.Name = "kelas"
+        Me.kelas.Width = 75
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btn_tambah)
+        Me.GroupBox1.Controls.Add(Me.btn_cancel)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.btn_delete)
+        Me.GroupBox1.Controls.Add(Me.btn_update)
+        Me.GroupBox1.Controls.Add(Me.btn_add)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.txt_kelas)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.txt_prodi)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txt_jurusan)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txt_nama)
+        Me.GroupBox1.Controls.Add(Me.DataGridView1)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txt_npm)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 19)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.GroupBox1.Size = New System.Drawing.Size(1524, 726)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Data Mahasiswa"
+        '
+        'btn_tambah
+        '
+        Me.btn_tambah.Location = New System.Drawing.Point(1255, 381)
+        Me.btn_tambah.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_tambah.Name = "btn_tambah"
+        Me.btn_tambah.Size = New System.Drawing.Size(146, 53)
+        Me.btn_tambah.TabIndex = 21
+        Me.btn_tambah.Text = "Tambah Data"
+        Me.btn_tambah.UseVisualStyleBackColor = True
+        '
+        'btn_cancel
+        '
+        Me.btn_cancel.Location = New System.Drawing.Point(1255, 461)
+        Me.btn_cancel.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.btn_cancel.Name = "btn_cancel"
+        Me.btn_cancel.Size = New System.Drawing.Size(146, 57)
+        Me.btn_cancel.TabIndex = 20
+        Me.btn_cancel.Text = "Cancel"
+        Me.btn_cancel.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(30, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 23)
+        Me.Label1.TabIndex = 19
+        Me.Label1.Text = "Cari Data"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 553)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 23)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "NPM"
+        '
+        'txt_npm
+        '
+        Me.txt_npm.Location = New System.Drawing.Point(17, 582)
+        Me.txt_npm.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.txt_npm.Name = "txt_npm"
+        Me.txt_npm.Size = New System.Drawing.Size(241, 30)
+        Me.txt_npm.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(163, 54)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(1071, 30)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1427, 659)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents btn_delete As Button
+    Friend WithEvents btn_update As Button
+    Friend WithEvents btn_add As Button
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txt_kelas As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txt_prodi As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txt_jurusan As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txt_nama As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents nomor As DataGridViewTextBoxColumn
+    Friend WithEvents npm As DataGridViewTextBoxColumn
+    Friend WithEvents nama As DataGridViewTextBoxColumn
+    Friend WithEvents jurusan As DataGridViewTextBoxColumn
+    Friend WithEvents prodi As DataGridViewTextBoxColumn
+    Friend WithEvents kelas As DataGridViewTextBoxColumn
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txt_npm As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents btn_tambah As Button
+    Friend WithEvents btn_cancel As Button
+End Class
---
+++ Vb CRUD Api/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+    Module Module1
+        Public con As New ADODB.Connection
+        Public rec As New ADODB.Recordset
+
+        Public Sub buka()
+            con.Open("DSN=pembayaran_vb")
+        End Sub
+        Public Sub tutup()
+            con.Close()
+        End Sub
+    End Module
+End Module
